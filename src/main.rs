876462979be99b3f6c852ba4fbf580389ba0c96b
Jeffrey Yu <wehjin@gmail.com>
2016-08-21 07:25:46 -0700

    glewInit

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,41 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::video::GLProfile;
+
+extern crate libc;
+
+#[link(name = "GLEW")]
+#[allow(non_snake_case)]
+extern {
+    static mut glewExperimental: libc::c_int;
+    fn glewInit() -> i32;
+}
+
+fn glew_init() -> i32 {
+    unsafe {glewExperimental = 1};
+    return unsafe { glewInit() };
+}
 
 pub fn main() {
     println!("Main!!");
 
     let sdl_context = sdl2::init().unwrap();
-
     let video_subsystem = sdl_context.video().unwrap();
-
     let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
         .position_centered()
         .opengl()
         .build()
         .unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_major_version(3);
+    gl_attr.set_context_minor_version(2);
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_double_buffer(true);
+    video_subsystem.gl_set_swap_interval(1);
+    glew_init();
 
     let mut renderer = window.renderer().build().unwrap();
 
