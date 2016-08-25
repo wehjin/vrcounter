e21eaf506848237ac6fa98a3d2b40742b09fc356
Jeffrey Yu <wehjin@gmail.com>
2016-08-25 07:06:46 -0700

    Use model-update-view architecture.

---
+++ src/app.rs
@@ -0,0 +1,94 @@
+extern crate glium;
+
+use glium::{DisplayBuild, Surface};
+use glium::glutin::{Event, ElementState};
+use world;
+use cam;
+
+pub struct Model {
+    display: glium::Display,
+    room: world::Room,
+    camera: cam::Camera,
+}
+
+impl Model {
+    pub fn init() -> Model {
+        let display: glium::Display = glium::glutin::WindowBuilder::new()
+            .with_title("vrcounter")
+            .with_depth_buffer(24)
+            .build_glium()
+            .unwrap();
+        let room = world::Room::for_display(&display);
+        let camera = cam::Camera::start();
+        Model { display: display, room: room, camera: camera }
+    }
+
+    pub fn with_camera(self, camera: cam::Camera) -> Model {
+        Model { display: self.display, room: self.room, camera: camera }
+    }
+}
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Near,
+    Far
+}
+
+pub enum Message {
+    Quit,
+    Move(Direction),
+    Reset,
+}
+
+pub fn update(message: &Message, model: Model) -> Option<Model> {
+    match *message {
+        Message::Quit => None,
+        Message::Reset => Some(model.with_camera(cam::Camera::start())),
+        Message::Move(ref direction) => {
+            let camera = match *direction {
+                Direction::Up => model.camera.up(),
+                Direction::Down => model.camera.down(),
+                Direction::Left => model.camera.left(),
+                Direction::Right => model.camera.right(),
+                Direction::Near => model.camera.near(),
+                Direction::Far => model.camera.far(),
+            };
+            Some(model.with_camera(camera))
+        }
+    }
+}
+
+pub fn view(model: &Model) -> Message {
+    let mut target = model.display.draw();
+    target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+    model.room.draw(&mut target, &model.camera);
+    target.finish().unwrap();
+    let mut message_option: Option<Message> = None;
+    while message_option.is_none() {
+        for glutin_event in model.display.poll_events() {
+            message_option = match glutin_event {
+                Event::Closed => Some(Message::Quit),
+                Event::KeyboardInput(ElementState::Pressed, 53, _) => Some(Message::Quit),
+                Event::KeyboardInput(ElementState::Pressed, 47, _) => Some(Message::Reset),
+                Event::KeyboardInput(ElementState::Pressed, 1, _) => Some(Message::Move(Direction::Down)),
+                Event::KeyboardInput(ElementState::Pressed, 13, _) => Some(Message::Move(Direction::Up)),
+                Event::KeyboardInput(ElementState::Pressed, 0, _) => Some(Message::Move(Direction::Left)),
+                Event::KeyboardInput(ElementState::Pressed, 2, _) => Some(Message::Move(Direction::Right)),
+                Event::KeyboardInput(ElementState::Pressed, 12, _) => Some(Message::Move(Direction::Far)),
+                Event::KeyboardInput(ElementState::Pressed, 14, _) => Some(Message::Move(Direction::Near)),
+                Event::KeyboardInput(ElementState::Pressed, code, _) => {
+                    println!("{}", code);
+                    None
+                },
+                _ => None
+            };
+            if message_option.is_some() {
+                break;
+            }
+        }
+    }
+    return message_option.unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,15 @@
 mod world;
 mod mat;
 mod cam;
-
-use glium::glutin::{Event, ElementState};
+mod app;
 
 fn main() {
-    use glium::{DisplayBuild, Surface};
-    let display: glium::Display = glium::glutin::WindowBuilder::new()
-        .with_title("vrcounter")
-        .with_depth_buffer(24)
-        .build_glium()
-        .unwrap();
-
-    let room = world::Room::for_display(&display);
-    let mut camera = cam::Camera::start();
+    let mut model = app::Model::init();
     loop {
-        let mut target = display.draw();
-        target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
-        room.draw(&mut target, &camera);
-        target.finish().unwrap();
-        for ev in display.poll_events() {
-            match ev {
-                Event::Closed => return,
-                Event::KeyboardInput(ElementState::Pressed, 53, _) => return,
-                Event::KeyboardInput(ElementState::Pressed, 1, _) => {
-                    camera = camera.down();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 13, _) => {
-                    camera = camera.up();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 0, _) => {
-                    camera = camera.left();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 2, _) => {
-                    camera = camera.right();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 47, _) => {
-                    camera = cam::Camera::start();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 12, _) => {
-                    camera = camera.far();
-                },
-                Event::KeyboardInput(ElementState::Pressed, 14, _) => {
-                    camera = camera.near();
-                },
-                Event::KeyboardInput(ElementState::Pressed, code, _) => println!("{}", code),
-                _ => ()
-            }
+        let message = app::view(&model);
+        match app::update(&message, model) {
+            None => return,
+            Some(next_model) => model = next_model,
         }
     }
 }
