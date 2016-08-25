b8a735db7eab93d25abd8d7a9e1386627a629bd8
Jeffrey Yu <wehjin@gmail.com>
2016-08-24 23:05:06 -0700

    Add camera controls.

--- src/cam.rs
+++ src/cam.rs
@@ -1,3 +1,7 @@
+use mat;
+
+static STEP: f32 = 0.04;
+
 pub struct Camera {
     pub eye: [f32; 3],
     pub look: [f32; 3],
@@ -5,7 +9,31 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn make() -> Camera {
+    pub fn start() -> Camera {
         Camera { eye: [0.0, 1.6, 0.0], look: [0.0, 0.0, -1.0], up: [0.0, 1.0, 0.0] }
+    }
+
+    pub fn up(&self) -> Camera {
+        Camera { eye: self.eye, look: mat::add_y(&self.look, STEP), up: self.up }
+    }
+
+    pub fn down(&self) -> Camera {
+        Camera { eye: self.eye, look: mat::add_y(&self.look, -STEP), up: self.up }
+    }
+
+    pub fn left(&self) -> Camera {
+        Camera { eye: self.eye, look: mat::add_x(&self.look, -STEP), up: self.up }
+    }
+
+    pub fn right(&self) -> Camera {
+        Camera { eye: self.eye, look: mat::add_x(&self.look, STEP), up: self.up }
+    }
+
+    pub fn near(&self) -> Camera {
+        Camera { eye: mat::add_z(&self.eye, -STEP), look: self.look, up: self.up }
+    }
+
+    pub fn far(&self) -> Camera {
+        Camera { eye: mat::add_z(&self.eye, STEP), look: self.look, up: self.up }
     }
 }
