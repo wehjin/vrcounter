5e94f422764dd2677b9e542d090ffbda7b0e2dab
Jeffrey Yu <wehjin@gmail.com>
2016-08-27 10:42:36 -0700

    Await poses.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub mod app;
 #[derive(Debug)]
 pub enum Error {
     NoSystem,
+    NoCompositor,
 }
 
 #[derive(Debug)]
@@ -25,23 +26,40 @@ impl From<openvr::common::Size> for RenderSize {
     }
 }
 
+#[derive(Debug)]
+pub struct Poses {
+    poses: openvr::tracking::TrackedDevicePoses
+}
+
+impl From<openvr::tracking::TrackedDevicePoses> for Poses {
+    fn from(poses: openvr::tracking::TrackedDevicePoses) -> Self {
+        Poses {poses: poses}
+    }
+}
+
 pub struct System {
-    ovr: openvr::system::IVRSystem,
+    system: openvr::system::IVRSystem,
+    compositor: openvr::compositor::IVRCompositor,
 }
 
 impl System {
     pub fn up() -> Result<System, Error> {
-        let ovr = try!(openvr::init().map_err(|_| Error::NoSystem));
-        Ok(System { ovr: ovr })
+        let system = try!(openvr::init().map_err(|_| Error::NoSystem));
+        let compositor = try!(openvr::subsystems::compositor().map_err(|_| Error::NoCompositor));
+        Ok(System { system: system, compositor: compositor })
     }
 
     pub fn get_render_size(&self) -> RenderSize {
-        let size = self.ovr.recommended_render_target_size();
+        let size = self.system.recommended_render_target_size();
         RenderSize::from(size)
     }
 
-    pub fn get_name(&self) -> String {
-        String::from("hey")
+    pub fn get_can_render(&self) -> bool {
+        self.compositor.can_render_scene()
+    }
+
+    pub fn await_poses(&self) -> Poses {
+        Poses::from(self.compositor.wait_get_poses())
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ fn main() {
     let vr_option = lib::System::up().ok();
     if vr_option.is_some() {
         let vr = vr_option.unwrap();
+
         let render_size = vr.get_render_size();
-        println!("{:?}", render_size)
+        println!("{:?}", render_size);
+
+        let can_render = vr.get_can_render();
+        println!("Can render {}", can_render);
+
+        let poses = vr.await_poses();
+        println!("{:?}", poses);
     }
 
     let mut model = lib::app::Model::init();
