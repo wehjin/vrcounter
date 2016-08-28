ff07550cf95db1123d7eadfd3c6d5e3438a912c2
Jeffrey Yu <wehjin@gmail.com>
2016-08-28 09:08:38 -0700

    Add color to shape

--- src/patchprogram.rs
+++ src/patchprogram.rs
@@ -10,8 +10,12 @@ use glium::index::{NoIndices, PrimitiveType};
 struct Vertex {
     position: [f32; 3],
     normal: [f32; 3],
+    color: [f32; 4],
 }
-implement_vertex!(Vertex, position, normal);
+implement_vertex!(Vertex, position, normal, color);
+
+static RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+static GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 
 struct ShapeList {
     shapes: [Shape; 2],
@@ -20,8 +24,8 @@ struct ShapeList {
 
 impl ShapeList {
     fn new() -> Self {
-        let shape1 = Shape::new(-0.5, 0.5, 0.25, -0.25, 0.0, 0);
-        let shape2 = Shape::new(0.25, 0.75, 0.5, 0.0, -0.10, 1);
+        let shape1 = Shape::new(-0.5, 0.5, 0.25, -0.25, 0.0, RED, 0);
+        let shape2 = Shape::new(0.25, 0.75, 0.5, 0.0, -0.10, GREEN, 1);
         ShapeList { shapes: [shape1, shape2], full_count: 2 }
     }
 
@@ -42,19 +46,31 @@ struct Shape {
     bottom: f32,
     near: f32,
     normal: [f32; 3],
+    color: [f32; 4],
     index: i32,
 }
 
 impl Shape {
-    fn new(left: f32, right: f32, top: f32, bottom: f32, near: f32, index: i32) -> Self {
-        Shape { left: left, right: right, top: top, bottom: bottom, near: near, normal: [0.0, 0.0, -1.0], index: index }
+    fn new(left: f32, right: f32, top: f32, bottom: f32, near: f32, color: [f32; 4], index: i32) -> Self {
+        Shape {
+            left: left, right: right,
+            top: top, bottom: bottom,
+            near: near,
+            normal: [0.0, 0.0, -1.0],
+            color: color,
+            index: index
+        }
+    }
+
+    fn get_vertex_with_position(&self, position: [f32; 3]) -> Vertex {
+        Vertex { position: position, normal: self.normal, color: self.color }
     }
 
     fn to_vertices(&self) -> Vec<Vertex> {
-        let bottom_left = Vertex { position: [self.left, self.bottom, self.near], normal: self.normal };
-        let bottom_right = Vertex { position: [self.right, self.bottom, self.near], normal: self.normal };
-        let top_left = Vertex { position: [self.left, self.top, self.near], normal: self.normal };
-        let top_right = Vertex { position: [self.right, self.top, self.near], normal: self.normal };
+        let bottom_left = self.get_vertex_with_position([self.left, self.bottom, self.near]);
+        let bottom_right = self.get_vertex_with_position([self.right, self.bottom, self.near]);
+        let top_left = self.get_vertex_with_position([self.left, self.top, self.near]);
+        let top_right = self.get_vertex_with_position([self.right, self.top, self.near]);
         vec![bottom_left, top_left, top_right, bottom_left, top_right, bottom_right]
     }
 }
@@ -109,6 +125,9 @@ static VERTEX_SHADER: &'static str = r#"
 
         in vec3 position;
         in vec3 normal;
+        in vec4 color;
+
+        out vec4 vColor;
 
         uniform mat4 perspective;
         uniform mat4 view;
@@ -117,15 +136,18 @@ static VERTEX_SHADER: &'static str = r#"
         void main() {
             mat4 modelview = view * model;
             gl_Position = perspective * modelview * vec4(position, 1.0);
+            vColor = color;
         }
     "#;
 
 static FRAGMENT_SHADER: &'static str = r#"
         #version 140
 
+        in vec4 vColor;
+
         out vec4 color;
 
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vColor;
         }
     "#;
