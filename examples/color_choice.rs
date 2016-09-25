adeb8b1b9d0a9e09e2f09fc4938be0f3e8648413
Jeffrey Yu <wehjin@gmail.com>
2016-09-24 21:58:36 -0700

    Remove unused id fields.

--- examples/color_choice.rs
+++ examples/color_choice.rs
@@ -7,7 +7,6 @@ use vrcounter::color::*;
 use std::sync::Arc;
 use std::cell::RefCell;
 use std::rc::Rc;
-use rand::random;
 use cage::{Frame};
 
 #[derive(Clone, Debug)]
@@ -23,8 +22,6 @@ struct App;
 
 #[derive(Clone)]
 struct Model {
-    patch_id: u64,
-    beat_id: u64,
     wailing: Rc<RefCell<Box<Wailing<TouchMsg>>>>,
 }
 
@@ -39,8 +36,6 @@ impl Star for App {
                                           .place_before(color_wail(RED, Frame::default()));
         let wailing = wail.summon();
         Model {
-            patch_id: random::<u64>(),
-            beat_id: random::<u64>(),
             wailing: Rc::new(RefCell::new(wailing))
         }
     }
