a02434caa6a8d0fc7765fe14d0109988f071aeeb
Jeffrey Yu <wehjin@gmail.com>
2017-01-13 06:45:22 -0800

    Add Caravel trait.

---
+++ examples/caravel.rs
@@ -0,0 +1,21 @@
+use traveller::{Traveller, ColorTraveller};
+
+pub trait Caravel<T: Traveller> {
+    fn embark(&self) -> T;
+}
+
+pub struct ColorCaravel {
+    color: [f32; 4],
+}
+
+impl ColorCaravel {
+    pub fn new(color: [f32; 4]) -> Self {
+        ColorCaravel { color: color }
+    }
+}
+
+impl Caravel<ColorTraveller> for ColorCaravel {
+    fn embark(&self) -> ColorTraveller {
+        ColorTraveller::new(self.color)
+    }
+}
--- examples/onepatch.rs
+++ examples/onepatch.rs
@@ -5,6 +5,7 @@ extern crate rand;
 mod screen_metrics;
 mod journal;
 mod traveller;
+mod caravel;
 
 use vrcounter::*;
 use cage::Cage;
@@ -28,12 +29,19 @@ pub struct App {
     app_message_writer: std::sync::mpsc::Sender<AppMessage>,
 }
 
+use caravel::Caravel;
+
 impl App {
-    fn new<T>(user_message_writer: std::sync::mpsc::Sender<UserMessage>, viewer: Viewer, mut traveller: T) -> Self
-        where T: Traveller + Send + 'static
+    fn new<C, T>(
+        user_message_writer: std::sync::mpsc::Sender<UserMessage>,
+        viewer: Viewer,
+        caravel: C
+    ) -> Self
+        where T: Traveller, C: Caravel<T> + Send + 'static
     {
         let (app_message_writer, app_message_reader) = std::sync::mpsc::channel();
         std::thread::spawn(move || {
+            let mut traveller = caravel.embark();
             let mut travel_and_patch = |screen_metrics: ScreenMetrics| {
                 let mut journal = PrimeJournal::new(screen_metrics);
                 traveller.travel(&mut journal);
@@ -65,9 +73,10 @@ fn main() {
     let cage = Cage::from((-0.5, 0.5, -1.5, 0.0, 0.0, 0.2));
     let screen_metrics = ScreenMetrics::new(cage, 0.03, 0.01);
 
-    let traveller = traveller::ColorTraveller::new(VIOLET);
+    use caravel::ColorCaravel;
+    let caravel = ColorCaravel::new(VIOLET);
     let (user_message_writer, user_message_reader) = std::sync::mpsc::channel();
-    let app = App::new(user_message_writer.clone(), viewer.clone(), traveller);
+    let app = App::new(user_message_writer.clone(), viewer.clone(), caravel);
     app.send(AppMessage::Go(screen_metrics));
 
     let (user_event_writer, user_event_reader) = std::sync::mpsc::channel();
