61f8b113fa0c6c94e30f951f238f17e85f66e3e3
Jeffrey Yu <wehjin@gmail.com>
2017-01-14 12:07:43 -0800

    Move dock_top to Caravel.

--- examples/caravel/color.rs
+++ examples/caravel/color.rs
@@ -1,6 +1,4 @@
 use caravel::Caravel;
-use caravel::top_dock::TopDockCaravel;
-use traveller::Traveller;
 use traveller::color::ColorTraveller;
 
 pub struct ColorCaravel {
@@ -16,13 +14,6 @@ impl Caravel<ColorTraveller> for ColorCaravel {
 impl ColorCaravel {
     pub fn new(color: [f32; 4]) -> Self {
         ColorCaravel { color: color }
-    }
-
-    pub fn dock_top<TopT, TopC>(self, top_units: f32, top_caravel: TopC)
-                                -> TopDockCaravel<ColorTraveller, Self, TopT, TopC>
-        where TopT: Traveller, TopC: Caravel<TopT>
-    {
-        TopDockCaravel::new(top_units, self, top_caravel)
     }
 }
 
--- examples/caravel/mod.rs
+++ examples/caravel/mod.rs
@@ -2,7 +2,15 @@ pub mod color;
 pub mod top_dock;
 
 use traveller::Traveller;
+use caravel::top_dock::TopDockCaravel;
+use std::marker::Sized;
 
 pub trait Caravel<T: Traveller> {
     fn embark(&self) -> T;
+
+    fn dock_top<TopT, TopC>(self, top_units: f32, top_caravel: TopC) -> TopDockCaravel<T, Self, TopT, TopC>
+        where TopT: Traveller, TopC: Caravel<TopT>, Self: Sized
+    {
+        TopDockCaravel::new(top_units, self, top_caravel)
+    }
 }
