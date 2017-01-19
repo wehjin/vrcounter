09d542e8aff7d2f070ea835cf553a3c58f1f8d90
Jeffrey Yu <wehjin@gmail.com>
2017-01-18 21:18:45 -0800

    Switch to glutin virtual keycodes.

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,4 +1,3 @@
-use os::is_windows;
 use glium::glutin::{Event, ElementState};
 
 pub enum Key {
@@ -6,76 +5,47 @@ pub enum Key {
     LookDown,
     LookRight,
     LookLeft,
-    LookFar,
-    LookNear,
+    StepBack,
+    StepForward,
     ResetLook,
-    H,
-    J,
-    K,
-    L,
-    RBracket,
-    LBracket,
+    HandLeft,
+    HandDown,
+    HandUp,
+    HandRight,
+    HandForward,
+    HandBack,
     Quit,
 }
 
-impl Key {
-    fn for_windows(glutin_event: &Event) -> Option<Self> {
-        match glutin_event {
-            &Event::Closed => Some(Key::Quit),
-            &Event::KeyboardInput(ElementState::Pressed, 1, _) => Some(Key::Quit),
-            &Event::KeyboardInput(ElementState::Pressed, 52, _) => Some(Key::ResetLook),
-            &Event::KeyboardInput(ElementState::Pressed, 30, _) => Some(Key::LookLeft),
-            &Event::KeyboardInput(ElementState::Pressed, 32, _) => Some(Key::LookRight),
-            &Event::KeyboardInput(ElementState::Pressed, 17, _) => Some(Key::LookUp),
-            &Event::KeyboardInput(ElementState::Pressed, 31, _) => Some(Key::LookDown),
-            &Event::KeyboardInput(ElementState::Pressed, 16, _) => Some(Key::LookFar),
-            &Event::KeyboardInput(ElementState::Pressed, 18, _) => Some(Key::LookNear),
-            &Event::KeyboardInput(ElementState::Pressed, code, _) => {
-                println!("{}", code);
-                None
-            },
-            _ => None
-        }
-    }
-    fn for_mac(glutin_event: &Event) -> Option<Self> {
-        match glutin_event {
-            &Event::Closed => Some(Key::Quit),
-            &Event::KeyboardInput(ElementState::Pressed, 53, _) => Some(Key::Quit),
-            &Event::KeyboardInput(ElementState::Pressed, 47, _) => Some(Key::ResetLook),
-            &Event::KeyboardInput(ElementState::Pressed, 1, _) => Some(Key::LookDown),
-            &Event::KeyboardInput(ElementState::Pressed, 13, _) => Some(Key::LookUp),
-            &Event::KeyboardInput(ElementState::Pressed, 0, _) => Some(Key::LookLeft),
-            &Event::KeyboardInput(ElementState::Pressed, 2, _) => Some(Key::LookRight),
-            &Event::KeyboardInput(ElementState::Pressed, 12, _) => Some(Key::LookFar),
-            &Event::KeyboardInput(ElementState::Pressed, 14, _) => Some(Key::LookNear),
-            &Event::KeyboardInput(ElementState::Pressed, 4, _) => Some(Key::H),
-            &Event::KeyboardInput(ElementState::Pressed, 38, _) => Some(Key::J),
-            &Event::KeyboardInput(ElementState::Pressed, 40, _) => Some(Key::K),
-            &Event::KeyboardInput(ElementState::Pressed, 37, _) => Some(Key::L),
-            &Event::KeyboardInput(ElementState::Pressed, 30, _) => Some(Key::RBracket),
-            &Event::KeyboardInput(ElementState::Pressed, 33, _) => Some(Key::LBracket),
-            &Event::KeyboardInput(ElementState::Pressed, code, _) => {
-                println!("{}", code);
-                None
-            },
-            _ => None
-        }
-    }
-}
-
-pub struct Keymap {
-    is_windows: bool,
-}
+pub struct Keymap {}
 
 impl Keymap {
     pub fn init() -> Self {
-        Keymap { is_windows: is_windows() }
+        Keymap {}
     }
     pub fn key_for_event(&self, glutin_event: &Event) -> Option<Key> {
-        if self.is_windows {
-            Key::for_windows(glutin_event)
-        } else {
-            Key::for_mac(glutin_event)
+        use glium::glutin::VirtualKeyCode;
+        match glutin_event {
+            &Event::Closed => Some(Key::Quit),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Escape)) => Some(Key::Quit),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Q)) => Some(Key::StepForward),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Z)) => Some(Key::StepBack),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::H)) => Some(Key::HandLeft),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::J)) => Some(Key::HandDown),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::K)) => Some(Key::HandUp),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::L)) => Some(Key::HandRight),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::RBracket)) => Some(Key::HandForward),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::LBracket)) => Some(Key::HandBack),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::S)) => Some(Key::LookDown),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::W)) => Some(Key::LookUp),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::A)) => Some(Key::LookLeft),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::D)) => Some(Key::LookRight),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(VirtualKeyCode::Period)) => Some(Key::ResetLook),
+            &Event::KeyboardInput(ElementState::Pressed, _, Some(virtual_key_code)) => {
+                println!("Virtual key {:?}", virtual_key_code);
+                None
+            },
+            _ => None,
         }
     }
 }
